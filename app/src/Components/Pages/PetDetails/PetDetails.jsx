c4171e4fabfb2d71a041258a4652c9e334a840dd
Vicra <honvramirez@gmail.com>
2023-02-10 13:14:27 -0600

    Pet details

--- app/src/Components/Pages/PetDetails/PetDetails.jsx
+++ app/src/Components/Pages/PetDetails/PetDetails.jsx
@@ -1,7 +1,33 @@
 import { Container } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const PetDetails = (props) => {
-    return <Container></Container>;
+    return (
+        <div className="wrapper">
+            <Container className="container-form">
+                <h1>Pet details</h1>
+                <Row xs={1} sm={2}>
+                    <Col>
+                        <div className="img-container">
+                            <img src="logo192.png" alt="" />
+                        </div>
+                    </Col>
+                    <Col>
+                        <strong>Nombre:</strong> Nombre
+                        <h5>Información:</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Id repellendus exercitationem, praesentium
+                            voluptate cumque est beatae perferendis dolores
+                            nobis et eaque ullam atque ut voluptatibus natus
+                            aperiam laborum, sapiente unde!
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default PetDetails;
--- app/src/Components/PetCard/PetCard.jsx
+++ app/src/Components/PetCard/PetCard.jsx
@@ -2,12 +2,17 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import { useHistory } from "react-router-dom";
 
 const PetCard = () => {
+    const history = useHistory();
+    function navigateToDetails() {
+        history.push("/pet-details");
+    }
     return (
         <Container md="auto">
             <Row>
-                <div className="img-container">
+                <div className="img-container img-pet-card">
                     <img src="logo192.png" alt="" />
                 </div>
             </Row>
@@ -17,7 +22,12 @@ const PetCard = () => {
                     className="justify-content-between"
                     aria-label="Toolbar with Button groups"
                 >
-                    <Button variant="outline-secondary">Detalles</Button>
+                    <Button
+                        variant="outline-secondary"
+                        onClick={navigateToDetails}
+                    >
+                        Detalles
+                    </Button>
                     <Button variant="success">Adoptar</Button>
                 </ButtonToolbar>
             </Row>
