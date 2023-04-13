967e15b089d489704bcbb2e067a6e3b5b1693c58
Vicra <honvramirez@gmail.com>
2023-04-13 14:53:17 -0600

    Adding theme

--- app/src/index.js
+++ app/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
